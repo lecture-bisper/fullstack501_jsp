45319f269ec0ea729dc4388132e851ef9948f7ed
lecture-bisper <bisper@outlook.kr>
2023-11-15 16:35:30 +0900

    11/15 - mvc2 방식 jsp 게시판 JSFunction 클래스 오타 수정.

--- jsp1114_mvcboard/src/main/java/com/bitc/jsp1114_mvcboard/utils/JSFunction.java
+++ jsp1114_mvcboard/src/main/java/com/bitc/jsp1114_mvcboard/utils/JSFunction.java
@@ -1,30 +1,42 @@
 package com.bitc.jsp1114_mvcboard.utils;
 
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.jsp.JspWriter;
 
+import java.io.PrintWriter;
+
 public class JSFunction {
-  public static void alertLocation(String msg, String url, JspWriter out) {
+  public static void alertLocation(String msg, String url, HttpServletResponse res) {
     try {
+      res.setContentType("text/html; charset=UTF-8");
+      PrintWriter out = res.getWriter();
+
       String script = "<script>";
-      script += "alert(" + msg + ");";
+      script += "alert('" + msg + "');";
       script += "location.href = '" + url + "';";
       script += "</script>";
 
-      out.print(script);
+      out.println(script);
     }
     catch (Exception e) {}
   }
 
-  public static void alertBack(String msg, JspWriter out) {
+  public static void alertBack(String msg, HttpServletResponse res) {
     try {
+      res.setContentType("text/html; charset=UTF-8");
+      PrintWriter out = res.getWriter();
+
       String script = "<script>";
-      script += "alert(" + msg + ");";
+      script += "alert('" + msg + "');";
       script += "history.back();";
       script += "</script>";
 
-      out.print(script);
+      out.println(script);
     }
-    catch (Exception e) {}
+    catch (Exception e) {
+      System.out.println("에러 발생");
+      System.out.println(e.getMessage());
+    }
   }
 }
 
